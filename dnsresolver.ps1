a1420cc019e8495868e45397829fe442e3e9712a
Austin Grupposo <56005213+AustinGrupposo@users.noreply.github.com>
2022-02-10 19:08:58 -0500

    Update dnsresolver.ps1

--- dnsresolver.ps1
+++ dnsresolver.ps1
@@ -1,4 +1,4 @@
-param($subnet, $dnsserver)
+param($subnet, $dns)
 For ($x=1,$x -lt 255; #x++){
-	Resolve-DnsName -DnsOnly "$subnet.$x" -Server $server -ErrorAction Ignore
+	Resolve-DnsName -DnsOnly "$subnet.$x" -Server $dns -ErrorAction Ignore
 }
